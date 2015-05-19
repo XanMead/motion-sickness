921b1b5bb4cb76898f643b826965fda5a310ae4d
XanMead <x.m.mead@gmail.com>
2015-05-19 12:27:39 -0400

    added conversion functions

--- inertia.js
+++ inertia.js
@@ -1,24 +1,22 @@
 /*                                                                                        
  *        __________    ______    ________   _________   ______    ______                      
  *       /\   _   _ \  /\  __ \  /\___  __\ /\___  ___\ /\   _ \  /\  __ \                       
- *       \ \ \/\ \/\ \ \ \ \/\ \ \/__/\ \   \/__/\ \__/ \ \ \/\ \ \ \ \/\ \                     
- *        \ \ \ \ \ \ \ \ \ \_\ \    \ \ \     _\_\ \__  \ \ \_\ \ \ \ \ \ \                     
- *         \ \_\ \_\ \_\ \ \_____\    \ \_\   /\_______\  \ \_____\ \ \_\ \ \                  
- *          \/_/\/_/\/_/  \/_____/     \/_/   \/_______/   \/_____/  \/_/\/_/                 
- *  ______   _________    ______    _    __   ______    _______    ______     ______              
+ *       \ \ \/\ \/\ \ \ \ \/\ \ \/__/\ \_/ \/__/\ \__/ \ \ \/\ \ \ \ \/\ \                     
+ *        \ \ \ \ \ \ \ \ \ \_\ \    \ \ \     _\_\ \___ \ \ \_\ \ \ \ \ \ \                     
+ *         \ \_\ \_\ \_\ \ \_____\    \ \_\   /\________\ \ \_____\ \ \_\ \ \                  
+ *          \/_/\/_/\/_/  \/_____/     \/_/   \/________/  \/_____/  \/_/\/_/                 
+ *  ______   _________    ______   __    __   ______    _______    ______    ______              
  * /\  ___\ /\___  ___\ /\  ____\ /\ \  | |  /\   _ \  /\  ____\  /\  ___\  /\  ___\              
  * \ \ \__/ \/__/\ \__/ \ \ \___/ \ \ \_| |  \ \ \/\ \ \ \ \___/  \ \ \__/  \ \ \__/            
  *  \ \__ \     \ \ \    \ \ \     \ \   _ \  \ \ \ \ \ \ \  ___\  \ \__  \  \ \__  \             
- *   \/__\ \    _\_\ \__  \ \ \____ \ \ \/\ \  \ \ \ \ \ \ \ \__/_  \/__\  \  \/__\  \            
- *    /\____\  /\_______\  \ \_____\ \ \_\ \_\  \ \_\ \_\ \ \_____\  /\_____\  /\_____\         
- *    \/____/  \/_______/   \/_____/  \/_/\/_/   \/_/\/_/  \/_____/  \/_____/  \/_____/
-                  
- *                                                                               
- *                                                                               
- *                                                                               
- *                                                                               
- *                                                                               
+ *   \/__\ \    _\_\ \___ \ \ \____ \ \ \/\ \  \ \ \ \ \ \ \ \__/_  \/__\  \  \/__\  \            
+ *    /\____\  /\________\ \ \_____\ \ \_\ \_\  \ \_\ \_\ \ \_____\  /\_____\  /\_____\         
+ *    \/____/  \/________/  \/_____/  \/_/\/_/   \/_/\/_/  \/_____/  \/_____/  \/_____/
+ *
+ *                                     ~BY XAN MEAD~
+ *
  */
+
 $(function() {
 	initializeMap();
 });
@@ -33,8 +31,8 @@ function initializeMap() {
 	var map = new google.maps.Map($('#map-canvas'), mapOptions);
 }
 
-// Equatorial radius, with the (inaccurate, but useful) assumption that the earth is perfectly spherical
-var EARTH_RADIUS;
+/* Equatorial radius, in meters, with the (inaccurate, but useful) assumption that the earth is perfectly spherical */
+var EARTH_RADIUS = 6371000;
 
 function calculateVelocity(latitude, elevation) {
 	// Find the hypotenuse, or distance from center of earth to object
@@ -46,6 +44,32 @@ function calculateVelocity(latitude, elevation) {
 	// Find lateral circumference, or the distance the object will travel in one rotational period (24 hours)
 	var periodDistance = 2 * Math.pi * axisDistance;
 	
-	// Return velocity, in meters/second
-	return periodDistance / 86400;
+	// Divide by number of seconds in a day, to get meters/second
+	var velocity = periodDistance / 86400;
+	return velocity;
+}
+
+/* meters/second -> feet/second */
+function mpsToFps(mps) {
+	// meters to feet
+	var fps = mps * 3.281;
+	return fps;
+}
+
+/* meters/second -> miles/hour */
+function mpsToMph(mps) {
+	// meters to miles
+	var mph = mps * (1/1609);
+	// seconds to hours
+	mph *= 3600;
+	return mph;
+}
+
+/* meters/second -> kilometers/hour */
+function mpsToKph(mps) {
+	// meters to kilometers
+	var kph = mps * (1/1000);
+	// second to hours
+	kph *= 3600;
+	return kph;
 }
